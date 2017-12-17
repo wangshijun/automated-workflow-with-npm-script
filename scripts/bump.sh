bbc41c21500508af628be7122328cb666873f0e3
wangshijun2010 <wangshijun2010@gmail.com>
2017-12-17 18:01:16 +0800

    add script to bump version intellegently

---
+++ scripts/bump.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# get major/minor/patch version to change
+version=`cat package.json| grep version | grep -v release | awk -F\" '{print $4}'`
+components=($(echo $version | tr '.' '\n'))
+major=${components[0]}
+minor=${components[1]}
+patch=${components[2]}
+
+release='patch';
+
+# decide which version to increment
+if [ $patch -ge 9 ]; then
+    if [ $minor -ge 9 ]; then
+        release='major'
+    else
+        release='minor'
+    fi
+else
+    release='patch'
+fi
+
+echo "major=$major, minor=$minor, patch=$patch, release=$release"
+
+# upgrade version
+npm version $release
+
+# sync tags with git
+git push
+git push --tags
+
